5ea3714f5f929bfad7082a2008df589b3f4aaeee
HalynaNikos <halynanikos@gmail.com>
2021-07-14 20:57:48 +0300

    added js files

---
+++ js/jsButtons.js
@@ -0,0 +1,28 @@
+let goBackButton = document.getElementById('jsGoBack');
+if (goBackButton)
+{
+    goBackButton.addEventListener('click', goBack);
+}
+
+function goBack(event)
+{
+    window.history.back();
+}
+
+// ------- go to product ------------
+let productsImg = document.getElementsByClassName('jsProductImg');
+for (let i = 0; i < productsImg.length; i++) {
+    productsImg[i].addEventListener('click', productDetails);
+}
+
+// productsImg.array.forEach(element => {
+// });
+
+// for (const iterator of object) {
+// }
+
+function productDetails(event)
+{
+    let product = event.target;
+    window.location = 'product.html?productName=' + product.alt;
+}
