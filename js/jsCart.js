88c928114cbacb19f15ceaa64374a61271488ecb
HalynaNikos <halynanikos@gmail.com>
2021-07-23 20:46:06 +0300

    last commit

--- js/jsCart.js
+++ js/jsCart.js
@@ -47,3 +47,57 @@ function clearCart(event)
 }
 
 // ----------------------- show cart --------------------------------
+let showCartBtn = document.getElementById('show-cart');
+showCartBtn.addEventListener('click', showCart);
+
+function showCart(event) {
+    let rows = '';
+    for (let i = 0; i < basket.length; i++) {
+        rows += `<tr>` +
+                    `<td>${ basket[i].name }</td>` +
+                    `<td>$${ basket[i].price }</td>` +
+                    `<td><input id="${basket[i].id}" class="basket-amount" type="number" min="0" value="${ basket[i].amount }" /></td>` +
+                    `<td>$<span id="priceByProduct-${basket[i].id}">${(basket[i].price * basket[i].amount).toFixed(2)}</span></td>` +
+                `</tr>`;
+
+    }
+
+    let basketTbl = document.getElementById('basket');
+    basketTbl.innerHTML = rows;
+
+    let basketAmountInpt = document.getElementsByClassName('basket-amount');
+    for (let i = 0; i < basketAmountInpt.length; i++) {
+        basketAmountInpt[i].addEventListener('input', priceByProduct);
+
+    }
+
+    totalPrice();
+}
+
+function priceByProduct(event) {
+    let newAmount = event.target.value;
+    let productId = event.target.id;
+
+    let product = basket.find(item => item.id === Number(productId));
+    if (product)
+    {
+        product.amount = newAmount;
+
+        let productSpan = document.getElementById(`priceByProduct-${ productId }`);
+        productSpan.innerHTML = (product.amount * product.price).toFixed(2);
+    }
+
+    totalPrice();
+}
+
+function totalPrice()
+{
+    let res = 0;
+
+    for (let i = 0; i < basket.length; i++) {
+        res += basket[i].price * basket[i].amount;
+    }
+
+    let totalCart = document.getElementById('total-cart');
+    totalCart.innerText = res.toFixed(2);
+}
